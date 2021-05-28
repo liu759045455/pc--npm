9ae3ce193b5d5d3f02ee29d7da094957bfec9d4a
liuhaoran <123213@qq.com>
2021-05-28 09:13:33 +0800

    提交

--- src/api/indexApi.js
+++ src/api/indexApi.js
@@ -1,31 +1,9 @@
 import axios from "./http";
 
-export function IndexList() {
-    return axios({
-        url: "/brigade/view/index",
-        method: "post"
-    })
-}
-
+// 首页轮播图
 export function PostBanners() {
     return axios({
         url: "/datalyg/view/banners",
         method: "post"
-    })
-}
-
-
-export function GetNews(data) {
-    return axios({
-        url: `/brigade/view/news`,
-        method: "post",
-        data
-    })
-}
-
-export function GetSpecial(type) {
-    return axios({
-        url: `/brigade/view/specialClass/${type}`,
-        method: "get",
     })
 }
